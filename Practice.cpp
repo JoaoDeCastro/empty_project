ece4e186f4453d4f849d40cf73f1a06ac57ba47d
JOAO H DECASTRO <66505539+JoaoDeCastro@users.noreply.github.com>
2021-03-18 23:03:19 -0400

    Add project files.

---
+++ Practice.cpp
@@ -0,0 +1,21 @@
+/*File to use as a practice
+
+
+Joao DeCastro
+
+File to use as a practice
+
+*/
+
+
+
+
+
+
+using namespace System;
+using namespace System::Threading;
+
+int main()
+{
+	Console::WriteLine("Hello World");
+}
