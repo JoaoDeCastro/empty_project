8add12d644ae59f9ad302b31a45109813ef0d861
JOAO H DECASTRO <66505539+JoaoDeCastro@users.noreply.github.com>
2021-03-20 17:59:01 -0400

    making 2 different threads, it worked the way I expected

--- Threading.cpp
+++ Threading.cpp
@@ -28,7 +28,8 @@ ref class User
 public:
 	User();
 	~User();
-	
+	//TODO
+	// get user input while printing documents
 
 	//setting user
 	void setUser(String^ user, Document type)
@@ -73,9 +74,12 @@ ref class Printer :  public User
 public:
 	Printer();
 	~Printer();
+
+	//each function will take 50 cycle to be executed
 	void print(User^ user, int copies);
 	void scan(User^ user);
 	void copy(User^ user, int copies);
+	void userInteraction();
 
 private:
 	int numberOfCopies;
@@ -94,9 +98,15 @@ Printer::~Printer()
 
 void Printer::print(User^ user, int copies)
 {
-	Console::WriteLine("Printing document");
+	Console::WriteLine("Printing document: ");
+	Console::Write("Document type: ");
 	Console::WriteLine(user->getDocument());
-	Console::WriteLine("Number of copies:{0}", copies);
+	Console::WriteLine("Number of copies: {0}", copies);
+	for (int i = 0; i <= 10000; i++)
+	{
+		Console::WriteLine("Printing...");
+	}
+	Console::WriteLine("Printing was done successfully");
 	
 	
 }
@@ -106,26 +116,69 @@ void Printer::scan(User^ user)
 {
 	Console::WriteLine("Scanning document");
 	Console::WriteLine(user->getDocument());
-	
+	for (int i = 0; i <= 100; i++)
+	{
+		if (i / 2 == 0)
+		{
+			Console::WriteLine("Scanning... ");
+		}
+
+	}
 
 }
 
 void Printer::copy(User^ user, int copies)
 {
 	Console::WriteLine("Copying document");
+	Console::Write("Document type: ");
 	Console::WriteLine(user->getDocument());
 	Console::WriteLine("Number of copies:{0}", copies);
+	for (int i = 0; i <= 50; i++)
+	{
+		Console::WriteLine("Copying... ");
+
+	}
+}
+
+void Printer::userInteraction()
+{
+	
+		Console::WriteLine("Do you want to print another document? ");
+		String^ answer;
+		answer = Console::ReadLine();
+
 }
 
 int main()
 {
-	Console::WriteLine("### WELCOME TO JHC PRINTER");
 
-	User^ joao = gcnew User();
-	joao->setUser("Joan", Picture);
 	
+	Console::WriteLine("### WELCOME TO JHC PRINTER");
+	// creating an instance of a printer
 	Printer^ printer1 = gcnew Printer();
+
+	//creating thread
+	// passing to Thread's constructor a delegate of type System::ThreadStart,
+	// which supports any function taking no arguments and returning no value
+	// it could encapsulated multiple functions
+	Thread^ t1 = gcnew Thread(gcnew ThreadStart(printer1, &Printer::userInteraction));
+	t1->Name = "t1";
+	Thread^ t2 = gcnew Thread(gcnew ThreadStart(printer1, &Printer::userInteraction));
+	t2->Name = "t2";
+	t2->Start();
+	
+
+	// creating an instance of user
+	User^ joao = gcnew User();
+	joao->setUser("Joao", Picture);
+	
+	
+	
 	printer1->print(joao, 5);
+	t1->Start();
+	
+	
+	
 
 	
 }
